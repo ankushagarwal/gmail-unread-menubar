0b44958626e0bc494e6d7387a52b3dc269ac3794
Ankush Agarwal <ankushagarwal11@gmail.com>
2022-05-23 22:42:48 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Gmail Unread Project
+
+This is a small project to pull the number of unread messages from various gmail labels. It is used to print them to xbar (mac menubar app)
+
+credentials.json comes from oauth google api credentials
+token.json is managed automatically
---
+++ gmail.py
@@ -0,0 +1,69 @@
+# https://developers.google.com/gmail/api/quickstart/python
+from __future__ import print_function
+
+import os.path
+
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+
+# If modifying these scopes, delete the file token.json.
+SCOPES = ['https://www.googleapis.com/auth/gmail.readonly']
+
+def search_messages(service, query):
+    result = service.users().messages().list(userId='me',q=query).execute()
+    messages = [ ]
+    if 'messages' in result:
+        messages.extend(result['messages'])
+    while 'nextPageToken' in result:
+        page_token = result['nextPageToken']
+        result = service.users().messages().list(userId='me',q=query, pageToken=page_token).execute()
+        if 'messages' in result:
+            messages.extend(result['messages'])
+    return messages
+
+def main():
+    """Shows basic usage of the Gmail API.
+    Lists the user's Gmail labels.
+    """
+    creds = None
+    # The file token.json stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists('token.json'):
+        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open('token.json', 'w') as token:
+            token.write(creds.to_json())
+
+    try:
+        # Call the Gmail API
+        service = build('gmail', 'v1', credentials=creds)
+        unread_dict = {}
+        total_unread = 0
+        labels = ['asana', 'cal', 'coda', 'docs', 'jira']
+        for label in labels:
+            unread_dict[label] = len(search_messages(service, f'label:{label} is:unread'))
+            total_unread += unread_dict[label]
+        print(f'G: {total_unread}')
+        print('---')
+        for label in labels:
+            print(f'{label}: {unread_dict[label]}')
+
+    except HttpError as error:
+        # TODO(developer) - Handle errors from gmail API.
+        print(f'An error occurred: {error}')
+
+
+if __name__ == '__main__':
+    main()
